cc09d15fab5e2e2fd16167ccb822044a4045dec4
BDDimitrov18 <58163537+BDDimitrov18@users.noreply.github.com>
2020-12-29 00:20:08 +0200

    UI Initial *funciton that displays string depending on the size of the screen *cool greeting message :)

--- kkb_project/kkb_project/kkb_project.cpp
+++ kkb_project/kkb_project/kkb_project.cpp
@@ -1,6 +1,59 @@
+
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <Windows.h>
+
+
 using namespace std;
+
+void centerstring(string s)
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbiInfo);
+    COORD NewSBSize;
+    NewSBSize.X = csbiInfo.dwSize.X;
+    int l = s.size();
+    int pos = (int)((NewSBSize.X - l) / 2);
+    for (int i = 0; i < pos; i++)
+        cout << " ";
+
+    cout << s;
+}
+
+
+
+
+
 int main()
 {
-    ;
+	string greeting = R"(
+ 
+ 
+         +--------------------------+
+         |   Welcome to bletchley   |
+         |                          |
+         |   +------------------+   |
+         |   | Choose an Option |   |
+         |   +------------------+   |                  
+         |                          |
+         +--------------------------+
+         |                          |
+         |   1.Play vs AI           |
+         |                          |
+         |   2.Play vs Player       |
+         |                          |
+         +--------------------------+  
+ 
+ 
+ 
+)";
+    string mitko = "asd";
+    centerstring(mitko);
+    cout << endl;
+    int a;
+    cin >> a;
+    centerstring(mitko);
+   
+    
 }
